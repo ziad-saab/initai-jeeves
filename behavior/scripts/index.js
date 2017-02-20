99539748929204b092e9945b3158c59a82b4dc63
Ziad Saab <ziad.saab@gmail.com>
2017-02-20 11:25:22 -0500

    first pass at question logic

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,35 +1,106 @@
-'use strict'
+'use strict';
+
+const MIN_CONFIDENCE = 0.15;
 
 exports.handle = (client) => {
+  // Helpers
+  function requireHuman() {
+    client.updateConversationState({
+      needsHuman: true
+    });
+    client.done();
+  }
+
+  function shouldWaitForNurse(state) {
+    return (
+      !state.questions
+      || state.needsHuman
+      || state.questions.every(q => !!q.answer)
+    );
+  }
+
   // Create steps
-  const sayHello = client.createStep({
+  const waitForNurse = client.createStep({
     satisfied() {
-      return Boolean(client.getConversationState().helloSent)
+      console.log('should wait?');
+      console.log ('should wait: ', shouldWaitForNurse(client.getConversationState()));
+      return !shouldWaitForNurse(client.getConversationState());
     },
 
     prompt() {
-      client.addResponse('welcome')
-      client.addResponse('provide/documentation', {
-        documentation_link: 'http://docs.init.ai',
-      })
-      client.addResponse('provide/instructions')
-
-      client.updateConversationState({
-        helloSent: true
-      })
-
-      client.done()
+      if (!client.getConversationState().questions) {
+        /*
+        @ NOTE: THIS PART IS ONLY HERE FOR TESTING!!!
+        Eventually, the questions will be added using an [Inbound Event](https://docs.init.ai/docs/events)
+        */
+        console.log('add questions');
+        client.updateConversationState({
+          questions: [
+            {
+              ask: 'do_you_smoke',
+              accept: ['affirmative', 'decline', 'smoking_answer']
+            },
+            {
+              ask: 'any_medications',
+              accept: ['affirmative', 'decline', 'medication_answer']
+            }
+          ]
+        });
+      }
+      client.done();
     }
-  })
+  });
 
-  const untrained = client.createStep({
+  const askQuestions = client.createStep({
     satisfied() {
-      return false
+      console.log('ask questions')
+      return client.getConversationState().questions.every(
+        q => typeof q.answer !== 'undefined'
+      );
     },
 
     prompt() {
-      client.addResponse('apology/untrained')
-      client.done()
+      const questions = client.getConversationState().questions;
+      const messagePart = client.getMessagePart();
+      const confidence = messagePart.confidence;
+
+      // If confidence is too low, signal the nurse
+      if (confidence < MIN_CONFIDENCE) {
+        console.log('too low conf', confidence, MIN_CONFIDENCE);
+        requireHuman();
+        return;
+      }
+
+      // Check if we were asking a question
+      const currentQuestion = questions.find(q => q.isAsking);
+      if (currentQuestion) {
+        const baseType = messagePart.classification.base_type.value;
+
+        // If we were asking a question, and the answer's classification is unexpected, signal the nurse
+        if (currentQuestion.accept && !currentQuestion.accept.includes(baseType)) {
+          console.log('not accepted', baseType, currentQuestion.accept);
+          requireHuman();
+          return;
+        }
+
+        // If we get here, then we have a satisfactory answer, move on!
+        currentQuestion.isAsking = false;
+        currentQuestion.answer = messagePart.content;
+      }
+
+      // Setup the next question if there is one
+      const nextQuestion = questions.find(q => !q.answer);
+      if (nextQuestion) {
+        nextQuestion.isAsking = true;
+        client.addResponse(`ask_question/${nextQuestion.ask}`);
+      }
+
+      // Update the convo state with any answers / new questions
+      client.updateConversationState({
+        questions
+      });
+
+      client.done();
     }
   })
 
@@ -37,13 +108,9 @@ exports.handle = (client) => {
     classifications: {
       // map inbound message classifications to names of streams
     },
-    autoResponses: {
-      // configure responses to be automatically sent as predicted by the machine learning model
-    },
     streams: {
-      main: 'onboarding',
-      onboarding: [sayHello],
-      end: [untrained],
+      main: 'loop',
+      loop: [waitForNurse, askQuestions]
     },
   })
 }
